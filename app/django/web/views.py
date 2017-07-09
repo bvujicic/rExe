8344083abfe95e5ecd3a546dcb8c2b298a0035f9
Boris Vujičić <boris.vujicic@broj42.com>
2017-07-09 17:37:23 +0200

    Logged in home.html

--- app/django/web/views.py
+++ app/django/web/views.py
@@ -8,5 +8,13 @@ class LandingView(LoginView):
     GET: Renders login form.
     POST: Processes form and logs user in.
     """
-    template_name = 'login.html'
+    #template_name = 'login.html'
     form_class = AuthenticationForm
+
+    def get_template_names(self):
+
+        if self.request.user.is_authenticated:
+            return 'login.html'
+
+        else:
+            return 'home.html'
