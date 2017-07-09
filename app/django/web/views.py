b03af265b9727441bbe6f45447abc61174b022ec
Boris Vujičić <boris.vujicic@broj42.com>
2017-07-09 17:17:19 +0200

    Landing page

--- app/django/web/views.py
+++ app/django/web/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+class LandingView(LoginView):
+    """
+    GET: Renders login form.
+    POST: Processes form and logs user in.
+    """
+    template_name = 'login.html'
