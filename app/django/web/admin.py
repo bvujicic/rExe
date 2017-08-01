ac37e52cb4f24ae0da4d39cdd6e0a6a8241e220e
Boris Vujičić <boris.vujicic@broj42.com>
2017-08-02 00:11:27 +0200

    Iteration Admin readonly_fields

--- app/django/web/admin.py
+++ app/django/web/admin.py
@@ -15,6 +15,7 @@ class IterationAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'input_data', 'output_data', 'status_code', 'algorithm', 'user')
     exclude = ('id',)
+    readonly_fields = ('status_code', 'status_message')
 
 
 @admin.register(LoginHistory)
