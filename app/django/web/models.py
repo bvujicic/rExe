83fbc7e43baf121928fd673f3b13644bba7ac2f6
Boris Vujičić <boris.vujicic@broj42.com>
2017-10-24 20:21:39 +0200

    Remove translation from models

--- app/django/web/models.py
+++ app/django/web/models.py
@@ -17,26 +17,26 @@ class Algorithm(TimestampModel):
     Information and binaries connected to an algorithm/application for remote execution.
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(verbose_name=_('naziv'), max_length=255, unique=True)
-    file = models.FileField(verbose_name=_('datoteka za izvršavanje'), upload_to=upload_path_exe)
-    description = models.TextField(verbose_name=_('opis'), blank=True)
-    document = models.FileField(verbose_name=_('datoteka dokumentacije'), blank=True, upload_to=upload_path_exe)
-    source = models.FileField(verbose_name=_('datoteka source kȏda'), blank=True, upload_to=upload_path_exe)
+    name = models.CharField(verbose_name='naziv', max_length=255, unique=True)
+    file = models.FileField(verbose_name='datoteka za izvršavanje', upload_to=upload_path_exe)
+    description = models.TextField(verbose_name='opis', blank=True)
+    document = models.FileField(verbose_name='datoteka dokumentacije', blank=True, upload_to=upload_path_exe)
+    source = models.FileField(verbose_name='datoteka source kȏda', blank=True, upload_to=upload_path_exe)
     is_active = models.BooleanField(
-        verbose_name=_('aktivno'),
+        verbose_name='aktivno',
         default=True,
-        help_text=_('Neaktivan algoritam se neće prikazivati kao opcija korisnicima.')
+        help_text='Neaktivan algoritam se neće prikazivati kao opcija korisnicima.'
     )
     auto_add = models.BooleanField(
-        verbose_name=_('automatski pristup korisnicima'),
+        verbose_name='automatski pristup korisnicima',
         default=True,
-        help_text=_('Omogućiti kako bi se novi registrirani korisnici automatski dodali u listu korisnika s pristupom.')
+        help_text='Omogućiti kako bi se novi registrirani korisnici automatski dodali u listu korisnika s pristupom.'
     )
-    users = models.ManyToManyField(to=settings.AUTH_USER_MODEL, verbose_name=_('korisnici s pristupom'), blank=True)
+    users = models.ManyToManyField(to=settings.AUTH_USER_MODEL, verbose_name='korisnici s pristupom', blank=True)
 
     class Meta:
-        verbose_name = _('algoritam')
-        verbose_name_plural = _('algoritmi')
+        verbose_name = 'algoritam'
+        verbose_name_plural = 'algoritmi'
         db_table = 'algorithm'
         ordering = ('-created',)
 
@@ -57,34 +57,34 @@ class Iteration(TimestampModel):
         (SUCCESS, _('uspješno završeno')),
     )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    input_data = models.FileField(verbose_name=_('ulazni podaci'), upload_to=upload_path_input_data, max_length=511)
-    output_data = models.FileField(verbose_name=_('izlazni podaci'), blank=True, max_length=511)
+    input_data = models.FileField(verbose_name='ulazni podaci', upload_to=upload_path_input_data, max_length=511)
+    output_data = models.FileField(verbose_name='izlazni podaci', blank=True, max_length=511)
     status_code = models.SmallIntegerField(
-        verbose_name=_('statusni kod'),
+        verbose_name='statusni kod',
         choices=STATUS_CHOICES,
         null=True,
         blank=True,
         default=START
     )
-    status_message = models.TextField(verbose_name=_('statusna poruka'), blank=True)
-    mail_on_completion = models.BooleanField(verbose_name=_('pošalji rezultate na mail'), default=False)
-    mailed = models.DateTimeField(verbose_name=_('poslano'), null=True)
-    finished = models.DateTimeField(verbose_name=_('završetak'), null=True)
+    status_message = models.TextField(verbose_name='statusna poruka', blank=True)
+    mail_on_completion = models.BooleanField(verbose_name='pošalji rezultate na mail', default=False)
+    mailed = models.DateTimeField(verbose_name='poslano', null=True)
+    finished = models.DateTimeField(verbose_name='završetak', null=True)
 
-    algorithm = models.ForeignKey(to='web.Algorithm', verbose_name=_('algoritam'), on_delete=models.PROTECT)
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, verbose_name=_('korisnik'), on_delete=models.PROTECT)
+    algorithm = models.ForeignKey(to='web.Algorithm', verbose_name='algoritam', on_delete=models.PROTECT)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, verbose_name='korisnik', on_delete=models.PROTECT)
 
     class Meta:
-        verbose_name = _('iteracija algoritma')
-        verbose_name_plural = _('iteracije algoritma')
+        verbose_name = 'iteracija algoritma'
+        verbose_name_plural = 'iteracije algoritma'
         db_table = 'algorithm_iteration'
         ordering = ('-created', 'algorithm')
 
     def __str__(self):
         return str(self.id)
 
-    @property
-    def input_is_image(self):
+    @staticmethod
+    def _is_image(file_data):
         from PIL import Image
 
         try:
@@ -99,20 +99,12 @@ class Iteration(TimestampModel):
             return True
 
     @property
+    def input_is_image(self):
+        return  self._is_image(file_data=self.input_data)
+
+    @property
     def output_is_image(self):
-        from PIL import Image
-
-        try:
-            image = Image.open(self.output_data)
-
-        except (OSError, ValueError):
-            # not an image
-            return False
-
-        else:
-            image.verify()
-            return True
-
+        return self._is_image(file_data=self.input_data)
 
     @property
     def started(self):
@@ -158,14 +150,14 @@ class LoginHistory(TimestampModel):
     """
     Simple login IP recording model.
     """
-    address = models.GenericIPAddressField(verbose_name=_('adresa logiranja'), protocol='both')
-    data = models.TextField(verbose_name=_('dodatni podaci'))
+    address = models.GenericIPAddressField(verbose_name='IP adresa prijave', protocol='both')
+    data = models.TextField(verbose_name='dodatni podaci')
 
-    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, verbose_name=_('korisnik'))
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, verbose_name='korisnik')
 
     class Meta:
-        verbose_name = _('unos povijesti logiranja')
-        verbose_name_plural = _('unosi povijesti logiranja')
+        verbose_name = 'unos povijesti logiranja'
+        verbose_name_plural = 'unosi povijesti logiranja'
         db_table = 'login_history'
         ordering = ('-created',)
 
